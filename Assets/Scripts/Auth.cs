f710e3cf473a647238f770521ba7d8bc5e834d18
Laura LM. Mezei <laura.mezei@gmail.com>
2022-09-20 10:02:08 +0200

    Lobby progress 1
    Adding ParrelSync, Authorization, Lobby and Relay packs,. Creating Anonym Authorization

---
+++ Assets/Scripts/Auth.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Unity.Services.Core;
+using Unity.Services.Authentication;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Auth : MonoBehaviour
+{
+    [SerializeField] private GameObject _buttons;
+
+    private async void Awake()
+    {
+        await UnityServices.InitializeAsync();
+    }
+
+    private static async Task SignInAnonymouslyAsync()
+    {
+        try
+        {
+            await AuthenticationService.Instance.SignInAnonymouslyAsync();
+            Debug.Log("Sign in anonymously succeeded!");
+
+            // Shows how to get the playerID
+            Debug.Log($"PlayerID: {AuthenticationService.Instance.PlayerId}");
+
+        }
+        catch (AuthenticationException ex)
+        {
+            // Compare error code to AuthenticationErrorCodes
+            // Notify the player with the proper error message
+            Debug.LogException(ex);
+        }
+        catch (RequestFailedException ex)
+        {
+            // Compare error code to CommonErrorCodes
+            // Notify the player with the proper error message
+            Debug.LogException(ex);
+        }
+    }
+
+     public static async void Buttonclick()
+    {
+        Debug.Log("You clicked the button");
+        await SignInAnonymouslyAsync();
+        SceneManager.LoadScene("Join-Make Game");
+    }
+
+}
