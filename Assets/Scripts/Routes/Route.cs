a9ba2705ab7b862fa63adf9e0ff4cfd3aed40d4b
Tobi8570 <73465176+Tobi8570@users.noreply.github.com>
2023-08-17 14:55:39 +0200

    Update Gameboard

---
+++ Assets/Scripts/Routes/Route.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Route : MonoBehaviour
+{
+
+    #region Variables
+
+    public GameObject route;
+
+    #endregion
+
+
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    
+}
