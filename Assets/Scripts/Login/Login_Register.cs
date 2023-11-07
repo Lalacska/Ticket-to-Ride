d343159f2dcc393909921007aab828784e31721d
Tobi8570 <73465176+Tobi8570@users.noreply.github.com>
2023-11-07 10:50:37 +0100

    Register scene working
    Fixed register; create new user/account, go to login scene.

--- Assets/Scripts/Login/Login_Register.cs
+++ Assets/Scripts/Login/Login_Register.cs
@@ -7,6 +7,8 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Networking;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+using UnityEngine.Rendering.VirtualTexturing;
 
 public class Login_Register : MonoBehaviour
 {
@@ -61,13 +63,12 @@ public class Login_Register : MonoBehaviour
 
         // This method starts the "Coroutine". \\
         StartCoroutine(LoginEnumerator());
-        //StartCoroutine(LoginUser(loginEmail, loginPassword));
-
     }
 
     // This method handles the users input and matches them to the database for login. \\
 
     
+    // Working. \\
     IEnumerator LoginEnumerator()
     {
         Debug.Log("b");
@@ -124,59 +125,6 @@ public class Login_Register : MonoBehaviour
     }
     
 
-    
-    IEnumerator LoginUser(string email, string password)
-    {
-        string url = "http://localhost/tickettoride/login.php"; // Replace with your server's URL
-
-        WWWForm form = new WWWForm();
-        form.AddField("email", email);
-        form.AddField("password", password);
-
-        using (UnityWebRequest www = UnityWebRequest.Post(url, form))
-        {
-            yield return www.SendWebRequest();
-
-            if (www.isNetworkError || www.isHttpError)
-            {
-                Debug.LogError("Login Error: " + www.error);
-            }
-            else
-            {
-                string responseText = www.downloadHandler.text;
-
-                if (responseText.Contains("Success"))
-                {
-                    // Check if the response contains "Success" indicating a successful login
-                    if (responseText.Contains("Success\n"))
-                    {
-                        int start = responseText.IndexOf("Success\n") + "Success\n".Length;
-                        string username = responseText.Substring(start).Trim();
-
-                        Debug.Log("Login Successful. Username: " + username);
-
-                        // Handle the successful login in Unity as needed
-                        // For example, you can save the username in a variable, load a new scene, open a URL, etc.
-                    }
-                    else
-                    {
-                        Debug.Log("Login Successful");
-
-                        // Handle other cases of success (if any)
-                    }
-                }
-                else
-                {
-                    Debug.Log("Login Failed");
-                    Debug.Log("Server Response: " + responseText);
-
-                    // Handle error messages sent by the PHP script in responseText
-                }
-            }
-        }
-    }
-    
-
 
     #endregion Login
 
@@ -210,10 +158,12 @@ public class Login_Register : MonoBehaviour
         form.AddField("username", registerUsername);
         form.AddField("password1", registerPassword1);
         form.AddField("password2", registerPassword2);
+        Debug.Log("a-register");
 
         using (UnityWebRequest www = UnityWebRequest.Post(rootURL + "register.php", form))
         {
             yield return www.SendWebRequest();
+            Debug.Log("b-register");
 
             if (www.result != UnityWebRequest.Result.Success)
             {
@@ -221,10 +171,14 @@ public class Login_Register : MonoBehaviour
             }
             else
             {
-                string responseText = www.downloadHandler.text;
+                string responseText = "Success" + www.downloadHandler.text;
+                Debug.Log("c-regitster");
+                Debug.Log(responseText);
 
                 if (responseText.StartsWith("Success"))
                 {
+                    Debug.Log("d-regitster");
+                    Debug.Log("User: " + registerUsername);
                     ResetValues();
                     registrationCompleted = true;
                     currentWindow = CurrentWindow.Login;
@@ -245,6 +199,42 @@ public class Login_Register : MonoBehaviour
     /// <summary>
     /// This region is for the extra methods in the script. \\
     /// </summary>
+    /// 
+
+
+    //  This part if needed, so we can use Tabulator to switch between the inputfeilds. \\
+    public EventSystem system;
+
+    public void Start()
+    {
+        system = EventSystem.current; // EventSystemManager.currentSystem;
+
+    }
+    // Update is called once per frame. \\
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            Selectable next = system.currentSelectedGameObject.GetComponent<Selectable>().FindSelectableOnDown();
+
+            if (next != null)
+            {
+
+                InputField inputfield = next.GetComponent<InputField>();
+                if (inputfield != null)
+                    inputfield.OnPointerClick(new PointerEventData(system));  //if it's an input field, also set the text caret
+
+                system.SetSelectedGameObject(next.gameObject, new BaseEventData(system));
+            }
+            //else Debug.Log("next nagivation element not found");
+
+        }
+        else
+        {
+
+        }
+    }
+
 
     void ResetValues()
     {
