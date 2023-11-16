9bf1c84ac09f205c2859bf4dca75d1f5f5f9992b
Lalacska <87847275+Lalacska@users.noreply.github.com>
2023-11-16 11:03:34 +0100

    Cleaning up code and adding comments

--- Assets/Scripts/Cards/Ticket.cs
+++ Assets/Scripts/Cards/Ticket.cs
@@ -9,31 +9,19 @@ using UnityEngine.Events;
 public class Ticket : MonoBehaviour
 {
     [SerializeField] private int m_ticketID;
-
     [SerializeField] private int m_ownerID;
-
     [SerializeField] private int m_score;
-
     [SerializeField] private string m_station1;
-
     [SerializeField] private string m_station2;
-
     [SerializeField] private bool m_isSpecial;
-
     [SerializeField] private bool m_isChosen;
 
     public int ticketID { get { return m_ticketID; } set { m_ticketID = value; } }
-
     public int ownerID { get { return m_ownerID; } set { m_ownerID = value; } }
-
     public int score { get { return m_score; } set { m_score = value; } }
-
     public string station1 { get { return m_station1; } set { m_station1 = value; } }
-
     public string station2 { get { return m_station2; } set { m_station2 = value; } }
-
     public bool isSpecial { get { return m_isSpecial; } set { m_isSpecial = value; } }
-
     public bool isChosen { get { return m_isChosen; } set { m_isChosen = value; } }
 
     private Material emissiveMaterial;
@@ -44,26 +32,26 @@ public class Ticket : MonoBehaviour
 
     void Update()
     {
+        // Cast a ray from the camera to the mouse position
         var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit Hit;
 
+        // Check for left mouse button click
         if (Input.GetMouseButtonDown(0))
         {
+            // Check if the ray hits the current game object
             if (Physics.Raycast(ray, out Hit) && Hit.collider.gameObject == gameObject)
             {
-                Debug.Log("Button Clicked " + ticketID);
+                // Toggle emission and log state
                 if (isChosen)
                 {
                     Debug.Log("Light off");
                     TurnEmissionOff();
-                    isChosen = false;
                 }else 
                 {
                     Debug.Log("Light on");
                     TurnEmissionOn(); 
-                    isChosen=true;
                 }
-                //Highlight.Instance.Enable_Disable();
                 OnClick.Invoke();
             }
         }
@@ -72,18 +60,21 @@ public class Ticket : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-        definedButton = this.gameObject;
-        emissiveMaterial = this.gameObject.GetComponent<Renderer>().material;
-        objectToChange = this.gameObject.GetComponent<Renderer>();
+        definedButton = gameObject;
+        emissiveMaterial = gameObject.GetComponent<Renderer>().material;
+        objectToChange = gameObject.GetComponent<Renderer>();
         emissiveMaterial.DisableKeyword("_EMISSION");
         isChosen = false;
     }
 
+    // Turn off emission
     public void TurnEmissionOff()
     {
         emissiveMaterial.DisableKeyword("_EMISSION");
         isChosen = false;
     }
+
+    // Turn on emission
     public void TurnEmissionOn()
     {
         emissiveMaterial.EnableKeyword("_EMISSION");
