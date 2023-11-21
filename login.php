b8ec4e391e442b132dbfa6476eb2abff89f096de
Daniel Nikolaj Hartwich <73463118+Drenzy@users.noreply.github.com>
2023-11-21 14:41:34 +0100

    Changed File structure

--- login.php
+++ login.php
@@ -82,7 +82,7 @@ if (isset($_POST["email"]) && isset($_POST["password"])) {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="styles/login-style.css">
-  <title>Document</title>
+  <title>Login</title>
 </head>
 
 <body>
