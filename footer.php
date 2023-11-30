484de96846639a00c76a6c80c42086b95b2ffc5e
Daniel Nikolaj Hartwich <73463118+Drenzy@users.noreply.github.com>
2023-11-30 09:40:20 +0100

    final touches

--- footer.php
+++ footer.php
@@ -40,8 +40,8 @@ if (!isset($_SESSION['user_id'])) {
             <!--We use "col-lg-6" because we are using a display over 1200px. The 6 stands for how many colums bootstrap need to generate-->
             <div class="col-lg-5">
                 <p>Ticket To Ride</p>
-                <a href="leaderboard.php">LeaderBoard</a>
-                <a href="forum.php">Forum</a>
+                <a href="index.php">LeaderBoard</a>
+                <a href="index.php">Forum</a>
                 <a href="contact.php">Contact</a>
             </div>
             <div class="col-lg-6">
